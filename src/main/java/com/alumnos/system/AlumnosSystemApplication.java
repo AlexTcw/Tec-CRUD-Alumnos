97dcd8524fa387bed9b1625b3769664ee0d23bc1
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-03-21 17:12:33 -0600

    push java project

---
+++ src/main/java/com/alumnos/system/AlumnosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alumnos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumnosSystemApplication.class, args);
+	}
+
+}
