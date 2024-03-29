955d41a54b45775088c0a0f2d2553c27d9763060
AlexTcw <etaverac429@alumno.uaemex.mx>
2024-03-29 13:42:21 -0600

    quito delete y pongo post

--- src/main/java/com/alumnos/system/controller/AlumnoController.java
+++ src/main/java/com/alumnos/system/controller/AlumnoController.java
@@ -3,17 +3,15 @@ package com.alumnos.system.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alumnos.system.Model.Alumno;
@@ -21,6 +19,7 @@ import com.alumnos.system.Model.StringLongDTO;
 import com.alumnos.system.Repository.AlumnoRepository;
 import com.alumnos.system.service.Alumno.AlumnoService;
 
+@CrossOrigin(origins = { "*" })
 @RestController
 @RequestMapping("/escuela/alumno")
 public class AlumnoController {
@@ -62,12 +61,12 @@ public class AlumnoController {
         return ResponseEntity.badRequest().body("No se pudo recuperar los Alumnos.");
     }
 
-    @DeleteMapping("delete")
+    @PostMapping("delete")
     public void deleteAlumnoById(@RequestBody StringLongDTO dto) {
         alumnoService.deleteAlumnoById(dto.getCve());
     }
 
-    @DeleteMapping("deleteCurso")
+    @PostMapping("deleteCurso")
     public ResponseEntity<Object> deleteCursoAlumno(@RequestBody StringLongDTO dto) {
         if (alumnoService.deleteCursoAlumno(dto.getCve(), dto.getLabel())) {
             return ResponseEntity.ok().body("Curso borrado exitosamente");
